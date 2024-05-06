e7a4fc03d9d91b8949e34ca0021cf4c95bec18a8
Eli Aiken <eliaikenpro@gmail.com>
2024-05-06 18:36:33 -0400

    Add data_types directory

---
+++ data_types/exercises.py
@@ -0,0 +1,19 @@
+'True'              #String,    'str'
+False               #Boolean,   'bool'
+(1, 2, 3)           #Tuple,     'tuple'
+1.5                 #Float,     'float'
+[1, 2, 3]           #List,      'list'
+2                   #Integer,   'int'
+range(5)            #Range,     'range'
+{1, 2, 3}           #Set,       'set' --> cannot have duplicates
+None                #Nonetype,  'nonetype'
+{'foo': 'bar'}      #Dictionary, 'dict'
+
+names = ('Asta', 'Butterscotch', 'Pudding', 'Neptune', 'Darwin')
+
+pets = {'Asta': 'dog',
+        'Butterscotch': 'cat', 
+        'Pudding': 'cat', 
+        'Neptune': 'fish',
+        'Darwin': 'lizard',
+        }
