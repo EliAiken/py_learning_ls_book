44fa00a94941720e56b98ee54cb727d79f48dacd
Eli Aiken <eliaikenpro@gmail.com>
2024-05-08 21:02:56 -0400

    Add exercises to repo

---
+++ variables/age.py
@@ -0,0 +1,13 @@
+your_age = 22
+print('You are ' + str(your_age) + ' years old.')
+print('In 10 years, you will be ' + str(your_age + 10) + ' years old.')
+print('In 20 years, you will be ' + str(your_age + 20) + ' years old.')
+print('In 30 years, you will be ' + str(your_age + 30) + ' years old.')
+print('In 40 years, you will be ' + str(your_age + 40) + ' years old.')
+
+age = 22
+print(f'You are {age} years old.')
+print(f'In 10 years, you will be {age + 10} years old.')
+print(f'In 20 years, you will be {age + 20} years old.')
+print(f'In 30 years, you will be {age + 30} years old.')
+print(f'In 40 years, you will be {age + 40} years old.')
---
+++ variables/greeter.py
@@ -0,0 +1,9 @@
+name = 'Victor'
+
+print(f'Good Morning, {name}.')
+print(f'Good Afternoon, {name}.')
+print(f'Good Evening, {name}.')
+
+print('Good Morning, ' + name + '.')
+print('Good Afternoon, ' + name + '.')
+print('Good Evening, ' + name + '.')
